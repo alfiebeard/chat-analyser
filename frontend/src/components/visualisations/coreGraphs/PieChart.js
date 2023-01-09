835db4ad5ef98f64d85e2178fa2e1bc1f8b672ac
alfiebeard <alfiebeard96@gmail.com>
2023-01-09 23:30:21 +0000

    Fixed colours

--- frontend/src/components/visualisations/coreGraphs/PieChart.js
+++ frontend/src/components/visualisations/coreGraphs/PieChart.js
@@ -1,9 +1,9 @@
 import React, {useState, useEffect} from 'react';
-import '../../app/App.css';
 import {Chart as Chartjs} from 'chart.js/auto';
 import {Chart} from 'chart.js';
 import {Pie} from 'react-chartjs-2';
 import { setGraphData, setOptions } from './utils';
+import NoDataChart from './NoDataChart';
 
 
 const PieChart = (props) => {
@@ -11,11 +11,13 @@ const PieChart = (props) => {
     <>
       {props.data ? 
         <Pie
-          data={setGraphData(props.data, props.x, props.y, props.label, props.x.length)}
+          data={setGraphData(props.data, props.x, props.y, props.label, props.data[props.x].length)}
           options={setOptions(props.title, props.xTitle, props.yTitle, false, false, false)}
         />
         :
-        null
+        <NoDataChart 
+          error={props.error}
+        />
       }
     </>
   );
