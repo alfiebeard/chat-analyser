2d414a7636fbea4d21de70d6410eabfeeff7251b
alfiebeard <alfiebeard96@gmail.com>
2023-01-13 18:33:51 +0000

    Added comma separation for numbers

--- frontend/src/components/information/GeneralStatistics.js
+++ frontend/src/components/information/GeneralStatistics.js
@@ -21,15 +21,15 @@ const GeneralStatistics = (props) => {
             {data.name}
           </div>
           <div>
-            <div><b>Total messages sent:</b> {data.total_messages}</div>
+            <div><b>Total messages sent:</b> {data.total_messages.toLocaleString()}</div>
             <div><b>Dates:</b> {data.start_date}-{data.end_date}</div>
             <div><b>Current Users:</b> {data.users.join(', ')}</div>
-            <div><b>Total photos:</b> {data.total_photos}</div>
-            <div><b>Total videos:</b> {data.total_videos}</div>
-            <div><b>Total calls:</b> {data.total_calls}</div>
+            <div><b>Total photos:</b> {data.total_photos.toLocaleString()}</div>
+            <div><b>Total videos:</b> {data.total_videos.toLocaleString()}</div>
+            <div><b>Total calls:</b> {data.total_calls.toLocaleString()}</div>
             <div><b>Average messages per day:</b> {Math.round((data.total_messages / (data.total_time / 86400)) * 100) / 100}</div>
             <div><b>Average message length:</b> {Math.round(data.mean_message_length * 100) / 100}</div>
-            <div><b>Longest silence:</b> {convertSeconds(data.longest_silence)}</div>
+            <div><b>Longest silence:</b> {convertSeconds(data.longest_silence).toLocaleString()}</div>
           </div>
         </>
         :
