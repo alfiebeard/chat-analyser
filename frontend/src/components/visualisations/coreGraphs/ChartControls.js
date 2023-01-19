ccbbb00db053d45c09f3fd1fe76f9b60b8ec217d
alfiebeard <alfiebeard96@gmail.com>
2023-01-19 00:11:35 +0000

    Hide chart controls when props not supplied

--- frontend/src/components/visualisations/coreGraphs/ChartControls.js
+++ frontend/src/components/visualisations/coreGraphs/ChartControls.js
@@ -12,55 +12,71 @@ const ChartControls = (props) => {
 
   return (
     <Row className={"w-100 chartControls"} style={{"display": props.displayControls ? "flex" : "none"}}>
-      <XFilter
-        name={props.xTitle}
-        dataX={props.dataX}
-        startXIndex={props.startXIndex}
-        setStartXIndex={props.setStartXIndex}
-        minXIndex={props.minXIndex}
-        endXIndex={props.endXIndex}
-        setEndXIndex={props.setEndXIndex}
-        maxXIndex={props.maxXIndex}
-      />
+      {typeof props.setStartXIndex !== 'undefined' && typeof props.setEndXIndex !== 'undefined' ?
+        <XFilter
+          name={props.xTitle}
+          dataX={props.dataX}
+          startXIndex={props.startXIndex}
+          setStartXIndex={props.setStartXIndex}
+          minXIndex={props.minXIndex}
+          endXIndex={props.endXIndex}
+          setEndXIndex={props.setEndXIndex}
+          maxXIndex={props.maxXIndex}
+        />
+        : null
+      }
 
-      <div className={"chartControlGroup"}>
-        <Form.Label className={"chartControlGroupItem"}>
-          Period
-        </Form.Label>
-        <Form.Select
-          className={"chartControlGroupItem chartControlSelect"}
-          value={props.xScale}
-          onChange={props.changeXScale}
+      {typeof props.xScale !== 'undefined' && typeof props.changeXScale !== 'undefined' && 
+        typeof props.xScaleOptions !== 'undefined' ?
+        <div className={"chartControlGroup"}>
+          <Form.Label className={"chartControlGroupItem"}>
+            Period
+          </Form.Label>
+          <Form.Select
+            className={"chartControlGroupItem chartControlSelect"}
+            value={props.xScale}
+            onChange={props.changeXScale}
+            size={"sm"}
+          >
+            {Object.keys(props.xScaleOptions).map((option) => 
+              <option key={option} value={option}>{props.xScaleOptions[option]}</option>
+            )}
+          </Form.Select>
+        </div>
+        : null
+      }
+
+      {typeof props.rescale !== 'undefined' && typeof props.changeRescale !== 'undefined' ?
+        <Form.Switch
+          className={"chartControl chartControlSwitch"}
+          checked={!props.rescale}
+          onChange={props.changeRescale}
+          label={"Rescale axis"}
+        />
+        : null
+      }
+
+      {typeof props.combineStacked !== 'undefined' && typeof props.changeCombineStacked !== 'undefined' ?
+        <Form.Switch
+          className={"chartControl chartControlSwitch"}
+          checked={!props.combineStacked}
+          onChange={props.changeCombineStacked}
+          label={"Split by user"}
+        />
+        : null
+      }
+
+      {typeof props.selectDeselectAll !== 'undefined' ?
+        <Button 
+          className={"chartControl chartControlButton"} 
           size={"sm"}
+          variant={"light"}
+          onClick={props.selectDeselectAll}
         >
-          {Object.keys(props.xScaleOptions).map((option) => 
-            <option key={option} value={option}>{props.xScaleOptions[option]}</option>
-          )}
-        </Form.Select>
-      </div>
-
-      <Form.Switch
-        className={"chartControl chartControlSwitch"}
-        checked={!props.rescale}
-        onChange={props.changeRescale}
-        label={"Rescale axis"}
-      />
-
-      <Form.Switch
-        className={"chartControl chartControlSwitch"}
-        checked={!props.combineStacked}
-        onChange={props.changeCombineStacked}
-        label={"Split by user"}
-      />
-
-      <Button 
-        className={"chartControl chartControlButton"} 
-        size={"sm"}
-        variant={"light"}
-        onClick={props.selectDeselectAll}
-      >
-        Select/Deselect all
-      </Button>
+          Select/Deselect all
+        </Button>
+        : null
+      }
     </Row>
   );
 }
